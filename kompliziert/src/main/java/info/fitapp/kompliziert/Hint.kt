5e2cd2fa61be3ab7a2a676507b518fe54875aac5
Markus Deutsch <md@karl-deutsch.at>
2019-01-14 08:16:55 +0100

    Fixed bubble alignment.

--- kompliziert/src/main/java/info/fitapp/kompliziert/Hint.kt
+++ kompliziert/src/main/java/info/fitapp/kompliziert/Hint.kt
@@ -27,39 +27,55 @@ class Hint(private val activity: Activity) : ViewTreeObserver.OnGlobalLayoutList
 
         val iconDimensPx = 50f * activity.resources.displayMetrics.densityDpi / DisplayMetrics.DENSITY_DEFAULT
 
+        /*
+         * Position the hint icon.
+         * If the bubble will be attached to the bottom, the icon needs to be aligned to the top and positioned with
+         * a top margin. Otherwise the icon needs to be aligned to the parent's bottom and positioned with a bottom
+         * margin.
+         */
+
         LayoutInflater.from(activity).inflate(R.layout.hint_icon, hintOverlay, true)
         val hintIcon = hintOverlay!!.findViewById<ImageView>(R.id.ivHintIcon)
         val hintIconLayoutParams = hintIcon.layoutParams as RelativeLayout.LayoutParams
+
+        // Calculate both margins.
         val topMargin = (centerTop - (iconDimensPx / 2)).roundToInt()
         val bottomMargin = hintOverlay!!.measuredHeight - centerTop - iconDimensPx / 2
+
+        // Position vertically.
         if (attachToBottom) {
-            hintIconLayoutParams.topMargin = topMargin
             hintIconLayoutParams.addRule(RelativeLayout.ALIGN_PARENT_TOP)
+            hintIconLayoutParams.topMargin = topMargin
         } else {
-            hintIconLayoutParams.bottomMargin = bottomMargin.roundToInt()
             hintIconLayoutParams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM)
+            hintIconLayoutParams.bottomMargin = bottomMargin.roundToInt()
         }
+
+        // Position horizontally.
         hintIconLayoutParams.leftMargin = (centerRight - iconDimensPx / 2).toInt()
 
-        // Also add the speech bubble.
+        /*
+         * Position the speech bubble either above or below the icon using RelativeLayout parameters.
+         */
+
         LayoutInflater.from(activity).inflate(R.layout.hint_bubble, hintOverlay, true)
         val bubbleLayout = hintOverlay!!.findViewById<LinearLayout>(R.id.llBubble)
         val bubbleLayoutParams = bubbleLayout.layoutParams as RelativeLayout.LayoutParams
+
         if (attachToBottom) {
-            //bubbleLayoutParams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM)
-            bubbleLayoutParams.addRule(RelativeLayout.ABOVE, hintIcon.id)
-        } else {
-            //bubbleLayoutParams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM)
             bubbleLayoutParams.addRule(RelativeLayout.BELOW, hintIcon.id)
+        } else {
+            bubbleLayoutParams.addRule(RelativeLayout.ABOVE, hintIcon.id)
         }
 
         // Offset the speech bubble pointer.
         val bubblePointerWidth = 28f * activity.resources.displayMetrics.densityDpi / DisplayMetrics.DENSITY_DEFAULT
         val pointerLayout =
-                hintOverlay!!.findViewById<ImageView>(if (attachToBottom) R.id.ivBubbleTipTop else R.id.ivBubbleTipBottom)
+            hintOverlay!!.findViewById<ImageView>(if (attachToBottom) R.id.ivBubbleTipTop else R.id.ivBubbleTipBottom)
         val pointerLayoutParams = pointerLayout.layoutParams as LinearLayout.LayoutParams
         pointerLayoutParams.marginStart = (centerRight - (bubblePointerWidth / 2)).toInt()
 
+        // Hide the pointer that is on the opposite side of the icon.
         hintOverlay!!.findViewById<ImageView>(R.id.ivBubbleTipBottom).visibility =
                 if (attachToBottom) View.GONE else View.VISIBLE
         hintOverlay!!.findViewById<ImageView>(R.id.ivBubbleTipTop).visibility =
