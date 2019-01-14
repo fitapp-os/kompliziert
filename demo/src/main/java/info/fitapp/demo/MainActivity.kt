73493d07be8d593ab951d30e56cc349f18fdb25c
Markus Deutsch <markus.deutsch@fitapp.info>
2019-01-14 15:49:39 +0100

    Updated sample project.

--- demo/src/main/java/info/fitapp/demo/MainActivity.kt
+++ demo/src/main/java/info/fitapp/demo/MainActivity.kt
@@ -2,7 +2,8 @@ package info.fitapp.demo
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import android.view.MotionEvent
 import info.fitapp.kompliziert.AnchorCoordinates
 import info.fitapp.kompliziert.Hint
@@ -11,6 +12,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
 
     private var hint: Hint? = null
+    private var showIcon = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,7 +20,6 @@ class MainActivity : AppCompatActivity() {
 
         root.setOnTouchListener { _, event ->
             if (event.action == MotionEvent.ACTION_DOWN) {
-                Log.d("MainActivity", "Touchdown detected.")
                 val anchor = AnchorCoordinates(event.x.toInt(), event.y.toInt())
                 hint = getFreshHint()
                 hint?.customAnchor = anchor
@@ -28,7 +29,31 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        if (item?.itemId == R.id.menu_toggle_star) {
+            showIcon = !showIcon
+            item.setIcon(if (showIcon) R.drawable.ic_action_no_star else R.drawable.ic_action_star)
+
+            if (hint?.isShowing == true) {
+                val lastAnchor = hint!!.customAnchor
+                hint!!.hide()
+
+                hint = getFreshHint()
+                hint?.customAnchor = lastAnchor
+                hint?.show()
+            }
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
     private fun getFreshHint(): Hint {
-        return Hint.withData(this, null, R.string.hint_title, R.string.hint_message, null, true)
+        val icon = if (showIcon) R.drawable.ic_star_white_38dp else null
+        return Hint.withData(this, null, R.string.hint_title, R.string.hint_message, icon, true)
     }
 }
